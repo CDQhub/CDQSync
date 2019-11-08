bb9a7dc99a201a7c5109324c743615c38095a4e3
CDQ <cdq-c@qq.com>
2019-11-08 18:44:08 +0800

    [feat]:更换Plug插件，新增了一些插件，更换了gruvbox的主题，对脚本进行了调整

--- bin/sync_bashrc.sh
+++ bin/sync_bashrc.sh
@@ -2,9 +2,6 @@
 
 cd ~
 
-# get Vundle
-git clone https://github.com/VundleVim/Vundle.vim.git ${HOME}/.vim/bundle/Vundle.vim
-
 # get bashrc
 wget https://raw.githubusercontent.com/CDQhub/CDQSync/master/conf/bashrc
 mv bashrc .bashrc
--- bin/sync_vimrc.sh
+++ bin/sync_vimrc.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 cd ~
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 wget https://raw.githubusercontent.com/CDQhub/CDQSync/master/conf/vimrc
-mv vimrc ~/.vimrc
+mv vimrc ~/.vim/vimrc
 vim +PluginInstall 
+cd -
