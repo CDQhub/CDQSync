f850f906589672d0536345c28f884676ee43159a
cdq <cdq-c@qq.com>
2019-11-13 22:10:15 +0800

    [fix]:修复之前shell脚本中的一些问题，以及修复zshrc中的bug

--- bin/sync_bashrc.sh
+++ bin/sync_bashrc.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-cd ~
+WORKDIR=$(dirname $(readlink -f "$0"))
+cd $WORKDIR
 
 # get bashrc
-wget https://raw.githubusercontent.com/CDQhub/CDQSync/master/conf/bashrc
-mv bashrc .bashrc
-source .bashrc
+# wget https://raw.githubusercontent.com/CDQhub/CDQSync/master/conf/bashrc
+# mv bashrc .bashrc
+cp ../conf/bashrc ~/.bashrc
+
+source ~/.bashrc
 cd -
--- bin/sync_vimrc.sh
+++ bin/sync_vimrc.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-cd ~
+WORKDIR=$(dirname $(readlink -f "$0"))
+cd $WORKDIR
+
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-wget https://raw.githubusercontent.com/CDQhub/CDQSync/master/conf/vimrc
-mv vimrc ~/.vim/vimrc
+# wget https://raw.githubusercontent.com/CDQhub/CDQSync/master/conf/vimrc
+# mv vimrc ~/.vim/vimrc
+cp ../conf/vimrc ~/.vim/vimrc
+
 vim +PluginInstall 
 cd -
