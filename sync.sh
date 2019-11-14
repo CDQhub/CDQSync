7b4eb39b15838186011ed7d9c07f694957f6d992
cdq <cdq-c@qq.com>
2019-11-14 11:28:00 +0800

    [fix]:修复之前shell脚本中的一些问题，以及修复zshrc中的bug

--- sync.sh
+++ sync.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+WORKDIR=$(dirname $(readlink -f "$0"))
+cd $WORKDIR
+
+echo "**********************"
 ./bin/sync_bashrc.sh
 echo "finish sync bashrc!"
+echo "**********************"
 ./bin/sync_zshrc.sh
 echo "finish sync zshrc!"
+echo "**********************"
 ./bin/sync_vimrc.sh
 echo "finish sync vimrc!"
+echo "**********************"
+
+cd -
